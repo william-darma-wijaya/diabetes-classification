04587efd8693fa261570e53231b4af4ed5923b3b
william-darma-wijaya <94919753+william-darma-wijaya@users.noreply.github.com>
2025-03-18 15:53:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,12 @@ def main():
   st.dataframe(df.head(50))
 
   st.subheader("Height vs Weight With Obesity Level")
-  
+  st.scatter_chart(
+    df,
+    x="Height",
+    y="Weight",
+    color="NObeyesdad"
+  )
 
   # input data by user
   erythema = st.slider("Erythema", min_value=0, max_value=3, value=2)
